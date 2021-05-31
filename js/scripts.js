c3ab2c503012c28f3977e3d6f63b493fb33214c9
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-05-31 16:53:18 +0200

    updates
    manifesto

--- js/scripts.js
+++ js/scripts.js
@@ -53,4 +53,17 @@
     // Collapse the navbar when page is scrolled
     $(window).on('scroll', navbarCollapse);
 
+  // slideshow
+
+    $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+$('#slideshow > div:first')
+.fadeOut(2000)
+.next()
+.fadeIn(3000)
+.end()
+.appendTo('#slideshow');
+},  5000);
+
 })(jQuery); // End of use strict
